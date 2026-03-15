8fd25da00562e7d084d71b02bc9fe8f3ac499945
cbwesterman <cbwesterman@icloud.com>
2026-03-15 13:42:37 -0400

    Fixed a problem with flux_err normalization

--- src/ingest.py
+++ src/ingest.py
@@ -7,8 +7,9 @@ def load_lightkurve(target, save_path=None):
         raise ValueError("No light curve data found for the specified target.")
     
     data = search_result.to_pandas()
-    data['flux'] = data['flux'] / data['flux'].median()
-    data['flux_err'] = data['flux_err'] / data['flux'].median()
+    flux_median = data['flux'].median()
+    data['flux'] = data['flux'] / flux_median
+    data['flux_err'] = data['flux_err'] / flux_median
 
     if save_path:
         data.to_csv(save_path)
